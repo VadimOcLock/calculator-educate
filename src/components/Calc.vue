982701465096def4447d3ad0f40ea739082facf8
Vadim Kozenkov <vadimoclock@gmail.com>
2022-10-24 21:33:12 +0300

    add calculator functions: clear, reverse, percent, dot, append

--- src/components/Calc.vue
+++ src/components/Calc.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="container">
-    <div class="display">234234</div>
-    <div class="btn">C</div>
-    <div class="btn">+/-</div>
-    <div class="btn">%</div>
+    <div class="display">{{ current || 0}}</div>
+    <div @click="clear()" class="btn">C</div>
+    <div @click="sign()" class="btn">+/-</div>
+    <div @click="percent()" class="btn">%</div>
     <div class="btn operator">&divide;</div>
-    <div class="btn">7</div>
-    <div class="btn">8</div>
-    <div class="btn">9</div>
+    <div @click="append(7)" class="btn">7</div>
+    <div @click="append(8)" class="btn">8</div>
+    <div @click="append(9)" class="btn">9</div>
     <div class="btn operator">&times;</div>
-    <div class="btn">4</div>
-    <div class="btn">5</div>
-    <div class="btn">6</div>
+    <div @click="append(4)" class="btn">4</div>
+    <div @click="append(5)" class="btn">5</div>
+    <div @click="append(6)" class="btn">6</div>
     <div class="btn operator">-</div>
-    <div class="btn">1</div>
-    <div class="btn">2</div>
-    <div class="btn">3</div>
+    <div @click="append(1)" class="btn">1</div>
+    <div @click="append(2)" class="btn">2</div>
+    <div @click="append(3)" class="btn">3</div>
     <div class="btn operator">+</div>
-    <div class="btn zero">0</div>
-    <div class="btn">.</div>
+    <div @click="append(0)" class="btn zero">0</div>
+    <div @click="dot()" class="btn">.</div>
     <div class="btn operator">=</div>
   </div>
 </template>
@@ -27,12 +27,28 @@
 export default {
   data() {
     return {
-      result: ""
+      current: "324",
 
     }
   },
   methods: {
-
+    clear: function () {
+      this.current = ""
+    },
+    sign: function () {
+      this.current = this.current.charAt(0) === "-" ?
+          this.current.slice(1) : `-${this.current}`
+    },
+    percent: function () {
+      this.current = `${ parseFloat(this.current) / 100 }`
+    },
+    append: function (number) {
+      this.current += number
+    },
+    dot: function () {
+      if (!this.current.includes('.'))
+        this.current += '.'
+    }
   }
 }
 </script>
@@ -66,6 +82,8 @@ export default {
 .btn
   padding: 15px
   border-radius: 50px
+  user-select: none
+  cursor: pointer
 
 .operator
   background-color: #4d4e52
